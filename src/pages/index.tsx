267f7bb2a3b4a4fe6673631571fe2af5056974b1
gt-engineer <gota.engineer@gmail.com>
2022-08-07 17:27:04 +0900

    feat: トップページのpage層を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,53 @@
 import NextHead from 'next/head'
+import NextScript from 'next/script'
 
-import { HomePage } from '@/components/page/HomePage'
+import { HomePage, HomePageQueryQuery, HomePageQueryDocument } from '@/components/page/HomePage'
 import { getGlobalLayout } from '@/components/shared/GlobalLayout'
 
 import { NextPageWithLayout } from './_app'
 
-const Home: NextPageWithLayout = () => {
+import { urqlClient } from '@/graphql/createClient'
+import MainVisual from '@/assets/main-visual.jpg'
+
+export const getServerSideProps = async () => {
+  const { data } = await urqlClient
+    .query<HomePageQueryQuery>(
+      HomePageQueryDocument,
+      {},
+      {
+        requestPolicy: 'cache-and-network',
+      },
+    )
+    .toPromise()
+  if (!data) throw new Error('エラーが発生しました')
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+const Home: NextPageWithLayout<{ data: HomePageQueryQuery }> = (props) => {
   return (
     <>
       <NextHead>
-        <title>ぽげちゃん STUDIO</title>
+        <title>ぽげラボ</title>
         <meta name="author" content="ぽげちゃん" />
         <meta
-          name="descirption"
-          content="ぽげちゃん STUDIOは歌ってみたのミックス依頼とホームページ作成依頼を承っています。"
+          name="description"
+          content="ぽげラボはお仕事としてWebエンジニアを、趣味として歌ってみたのMIXをする人のホームページです"
         />
-        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="ぽげラボ" />
+        <meta
+          property="og:description"
+          content="ぽげラボはお仕事としてWebエンジニアを、趣味として歌ってみたのMIXをする人のホームページです"
+        />
+        <meta property="og:image" content={MainVisual.src} />
+        <meta property="og:type" content="website" />
+        <NextScript async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
       </NextHead>
-      <HomePage />
+      <HomePage {...props} />
     </>
   )
 }
