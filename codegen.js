5082650554366f9bf811b33c9ae6e7bbf0b9d5a8
gt-engineer <gota.engineer@gmail.com>
2022-08-07 17:27:51 +0900

    chore: ライブラリ追加と設定追加

---
+++ codegen.js
@@ -0,0 +1,44 @@
+// overwrite: true
+// schema: "https://api.github.com/graphql"
+// documents: "src/**/*.graphql"
+// generates:
+//   src/generated/graphql.tsx:
+//     plugins:
+//       - "typescript"
+//       - "typescript-operations"
+//       - "typescript-react-apollo"
+//   ./graphql.schema.json:
+//     plugins:
+//       - "introspection"
+//   src/:
+//     preset: 'near-operation-file',
+const { parsed: env } = require('dotenv').config({
+  path: './.env',
+})
+
+module.exports = {
+  schema: {
+    'https://api.github.com/graphql': {
+      headers: {
+        Authorization: `bearer ${env.ACCESS_TOKEN}`,
+      },
+    },
+  },
+  overwrite: true,
+  documents: ['./src/**/*.graphql'],
+  generates: {
+    'src/graphql/generated/type.ts': { plugins: ['typescript'] },
+    'src/': {
+      preset: 'near-operation-file',
+      presetConfig: {
+        baseTypesPath: '~@/src/graphql/generated/type',
+      },
+      plugins: ['typescript-operations', 'typescript-urql', 'typescript-msw'],
+      config: {
+        withComponent: false,
+        withHOC: false,
+        withHooks: true,
+      },
+    },
+  },
+}
