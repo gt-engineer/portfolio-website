5401c3610a61f899c2905a7a6c94c937091c1c90
gt-engineer <gota.engineer@gmail.com>
2022-08-07 17:26:04 +0900

    feat: グローバルレイアウトの調整

--- src/components/shared/GlobalLayout/GlobalLayoutPresenter.tsx
+++ src/components/shared/GlobalLayout/GlobalLayoutPresenter.tsx
@@ -7,8 +7,6 @@ import { GlobalHeader } from './components/GlobalHeader'
 export const GlobalLayoutPresenter: FC<{ children: ReactNode }> = ({ children }) => (
   <>
     <GlobalHeader position="sticky" top="0" zIndex="600" />
-    <chakra.main maxW="810px" mx="auto" py="20" px="16" overflowX="hidden">
-      {children}
-    </chakra.main>
+    <chakra.main overflowX="hidden">{children}</chakra.main>
   </>
 )
--- src/components/shared/GlobalLayout/components/GlobalHeader/GlobalHeaderPresenter.tsx
+++ src/components/shared/GlobalLayout/components/GlobalHeader/GlobalHeaderPresenter.tsx
@@ -1,31 +1,37 @@
-import { FC, ReactNode } from 'react'
+import { ComponentProps, FC, ReactNode } from 'react'
 
 import NextLink from 'next/link'
 
 import { HTMLChakraProps, chakra, Tabs, Tab, TabList } from '@chakra-ui/react'
 
-export const GlobalHeaderPresenter: FC<HTMLChakraProps<'header'>> = ({ ...chakraProps }) => (
-  <chakra.header
-    py="12"
-    px="16"
-    backgroundColor="#fff"
-    borderBottom="1px solid #e5e5e5"
-    {...chakraProps}
-  >
-    <Tabs>
-      <TabList gap="12" justifyContent="center">
-        <TabItem idName="">TOP</TabItem>
-        <Tab>SCHEDULE</Tab>
-        <TabItem idName="works">WORKS</TabItem>
-        <Tab>PRICE</Tab>
-        <Tab>CONTACT</Tab>
-      </TabList>
-    </Tabs>
-  </chakra.header>
-)
+export const GlobalHeaderPresenter: FC<HTMLChakraProps<'header'>> = ({ ...chakraProps }) => {
+  return (
+    <chakra.header
+      py="12"
+      px="16"
+      backgroundColor="#fff"
+      borderBottom="1px solid #e5e5e5"
+      {...chakraProps}
+    >
+      <Tabs>
+        <TabList gap="16" justifyContent="center">
+          <TabItem idName="">TOP</TabItem>
+          <TabItem idName="schedule">SCHEDULE</TabItem>
+          <TabItem idName="works">WORKS</TabItem>
+          <TabItem idName="price">PRICE</TabItem>
+          <TabItem idName="contact">CONTACT</TabItem>
+        </TabList>
+      </Tabs>
+    </chakra.header>
+  )
+}
 
-const TabItem: FC<{ idName: string; children: ReactNode }> = ({ idName, children }) => (
-  <Tab>
+const TabItem: FC<{ idName: string; children: ReactNode } & ComponentProps<typeof Tab>> = ({
+  idName,
+  children,
+  ...tabProps
+}) => (
+  <Tab {...tabProps}>
     <NextLink href={`#${idName}`}>{children}</NextLink>
   </Tab>
 )
